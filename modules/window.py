d7797a4a68732fcf213bb45070e499f73af61fae
rainofpain <guestnewbie013@gmail.com>
2025-12-04 18:53:00 +0200

    screen positioned center for any resolution

--- modules/window.py
+++ modules/window.py
@@ -14,7 +14,7 @@ center_y = (screen_height // 2) - (screen_height // 4)
 
 main_window = widgets.QMainWindow()
 
-main_window.setGeometry(center_x, center_y, 1280, 800)
+main_window.setGeometry(center_x, center_y, screen_width // 2, screen_height // 2)
 main_window.setWindowTitle("Weather forecast")
 main_window.setStyleSheet("background-color: red; ")
 
